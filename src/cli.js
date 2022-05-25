6bc00abbb9b7e15fd5c674e0b53673495e4dc3d9
Cholym <ochab@DESKTOP-E6I83KG.localdomain>
2022-05-25 16:25:43 +0300

    correct errors from linter

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const sayHello = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, %s!', userName);
+const sayHello = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, %s!', userName);
 };
+
+export default sayHello;
