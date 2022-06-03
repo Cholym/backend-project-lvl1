f01c690b555246124b74db3a88bfa802e2ddd53c
Cholym <ochab@DESKTOP-E6I83KG.localdomain>
2022-06-04 01:16:49 +0300

    review 4 code refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,21 @@
-// eslint-disable-next-line import/no-named-default
-import { default as gameLogic, timesPlayToWin } from '../index.js';
+import gameLogic, { timesPlayToWin } from '../index.js';
 import generateRandomNumber from '../randomNumber.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => (number % 2 === 0);
 
+const generateRandomForOneRound = () => {
+  const minNumber = 1;
+  const maxNumberValue = 100;
+  const question = generateRandomNumber(minNumber, maxNumberValue);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
 const playGame = () => {
   const questionsAndAnswers = [];
-  for (let numberTries = 0; numberTries !== timesPlayToWin; numberTries += 1) {
-    const minNumber = 1;
-    const maxNumberValue = 100;
-    const question = generateRandomNumber(minNumber, maxNumberValue);
-    const correctAnswer = isEven(question) ? 'yes' : 'no';
-    questionsAndAnswers.push([question, correctAnswer]);
+  for (let numberTries = 0; numberTries < timesPlayToWin; numberTries += 1) {
+    questionsAndAnswers.push(generateRandomForOneRound());
   }
   gameLogic(rule, questionsAndAnswers);
 };
